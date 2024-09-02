662869607facda7d975e7534345eb281fb7cb7cc
Akash Kumar Amruth <111696048+AkashRabinal@users.noreply.github.com>
2024-09-02 18:45:31 +0000

    first commit 3rd sept 2024

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,50 @@
+"use strict" // treat all JS code as newer version
+// generally in call editors or node installed platforms automatically the code is treated with "use strict" mode by default even you dont use it
+
+//alert("hello")// we are using node JS not browser if you same code on browser it run the alert program not in node.js
+
+//use extension prettier which will make your code good
+
+// imporatence of code readability - always maintain good code readability
+
+//ECMA - https://ecma-international.org/
+//MDN Javascript - https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//datatypes in javascript
+
+//premitive data types
+
+//number => 2 to power of 53(not exactly- check online for reference( this is max memory space in computer where you store addresses)
+
+//web-While JavaScript can technically represent larger numbers, the maximum safe integer value is 2^53 - 1. 1  This is the largest integer that can be represented accurately without losing precision.
+
+//bigInt - generally when there is big numbers to presented will bigInt but we dont rarely use it  eg - stock exchange
+
+//string => ""
+//boolean => true/false
+//null => stand alone value
+// undefined => variable declared but not value
+//symbol => mostly will use while working on react componets as it "unique" symbols all componets
+
+
+//non-premitive datatypes
+
+//object
+
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
