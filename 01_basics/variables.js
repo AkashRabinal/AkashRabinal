cf6c89b00ef984f7f503c9bee811a8e4302b8682
Akash Kumar Amruth <111696048+AkashRabinal@users.noreply.github.com>
2024-09-02 18:02:31 +0000

    Second commit on 2nd Sept 2024

---
+++ 01_basics/variables.js
@@ -0,0 +1,30 @@
+ //video-4
+ const accountID = 144553
+ let accountEmail = "akashrabinal@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "Bengaluru"
+let accountState;//prints undefined
+
+//  accountID = 2
+//this below keys will overtakes let and var
+accountEmail = "h45@gmail.com"
+accountPassword = "12312124"
+accountCity = "Silicon city"
+
+
+ console.log(accountID);
+
+ console.table([accountID, accountState,accountEmail, accountPassword, accountCity]);
+
+//  {} - this space within the curly braces called scope
+
+/*
+this is also used commeting in javascript
+prefer not use var because of issue with block scope and functional scope
+*/
+
+//video5
+ 
+
+
+
